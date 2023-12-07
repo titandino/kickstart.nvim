14e199274125a19fa6bf6c5fcd9ab78c8a69db71
Trenton Kress <trenton.kress@gmail.com>
2023-12-07 04:30:06 -0800

    Add language servers

--- init.lua
+++ init.lua
@@ -562,12 +562,17 @@ require('mason-lspconfig').setup()
 --  If you want to override the default filetypes that your language server will attach to you can
 --  define the property 'filetypes' to the map in question.
 local servers = {
-  -- clangd = {},
-  -- gopls = {},
-  -- pyright = {},
-  -- rust_analyzer = {},
-  -- tsserver = {},
-  -- html = { filetypes = { 'html', 'twig', 'hbs'} },
+  eslint = {}
+  neocmake = {}
+  arduino_language_server = {}
+  java_language_server = {}
+  kotlin_language_server = {}
+  clangd = {},
+  gopls = {},
+  pyright = {},
+  rust_analyzer = {},
+  tsserver = {},
+  html = { filetypes = { 'html', 'twig', 'hbs'} },
 
   lua_ls = {
     Lua = {
